9abda862747033e5144b23ca72f4b51d783dae31
kenliew728 <fuiken28@hotmail.com>
2020-10-20 15:08:27 -0500

    Update schema SQL

--- schema.sql
+++ schema.sql
@@ -21,8 +21,9 @@ dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -51,9 +52,15 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT * FROM departments;
 
 SELECT * FROM employees;
+
+SELECT * FROM titles;
+
+SELECT * FROM dept_manager;
+
+DROP TABLE titles
